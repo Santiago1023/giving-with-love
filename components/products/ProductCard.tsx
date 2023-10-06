705a5b2116b30ed1ee3fc5efba0c21e3fb68ea9e
santiago1023 <santiago.bedoyad@udea.edu.co>
2023-10-06 17:53:23 -0500

    It add productList y productCard

---
+++ components/products/ProductCard.tsx
@@ -0,0 +1,36 @@
+import { IProduct } from '@/interfaces'
+import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { FC, useMemo, useState } from 'react'
+
+interface Props {
+    product: IProduct;
+}
+const ProductCard: FC<Props> = ({product}) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const productImage = useMemo(() => {
+        return isHovered ? `products/${product.images[1]}` : `products/${product.images[0]}` 
+    }, [isHovered, product.images])
+
+  return (
+    <Grid item xs={6} sm={4} key={product.slug} onMouseEnter={ () => setIsHovered(true)} onMouseLeave= { () => setIsHovered(false)}>
+        <Card>
+            <CardActionArea>
+                <CardMedia
+                    component='img'
+                    className='fadeIn'
+                    image={productImage}
+                    alt={product.title}
+                />
+            </CardActionArea>
+        </Card>
+
+        <Box sx={{ mt: 1}} className='fadeIn'>
+            <Typography fontWeight={700}>{product.title}</Typography>
+            <Typography fontWeight={500}>{`$${product.price}`}</Typography>
+        </Box>
+    </Grid>
+  )
+}
+
+export default ProductCard
---
+++ components/products/ProductList.tsx
@@ -0,0 +1,22 @@
+import { IProduct } from "@/interfaces"
+import { Grid } from "@mui/material"
+import { FC } from "react"
+import ProductCard from "./ProductCard";
+
+
+interface Props {
+    products: IProduct[];
+}
+const ProductList: FC<Props> = ({products}) => {
+  return (
+    <Grid container spacing={4}>
+        {
+            products.map(product => (
+                <ProductCard product={product} key={product.slug}/>
+            ))
+        }
+    </Grid>
+  )
+}
+
+export default ProductList

c71df14d3a838f753d97366566537d1b2b2ac8e4
santiago1023 <santiago.bedoyad@udea.edu.co>
2023-10-08 13:21:07 -0500

    Resume of order

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -2,14 +2,19 @@ import { initialData } from "@/database/products"
 import { Box, Button, CardActionArea, CardMedia, Grid, Link, Typography } from "@mui/material"
 import NextLink from 'next/link';
 import ItemCounter from "../ui/ItemCounter";
+import { FC } from "react";
 
 const productsInCart = [
     initialData.products[0],
     initialData.products[1],
     initialData.products[2],
-]
+];
 
-const CartList = () => {
+interface Props {
+    editable?: boolean;
+}
+
+const CartList: FC<Props> = ({editable= false}) => {
   return (
     <>
         {
@@ -33,16 +38,20 @@ const CartList = () => {
                         <Box display='flex' flexDirection='column'>
                             <Typography variant="body1">{product.title}</Typography>
                             <Typography variant="body1">Talla: <strong>M</strong> </Typography>
-                            {/* Condicional */}
-                            <ItemCounter/>
+                            {
+                                editable ? <ItemCounter/> : <Typography variant="h5">3 items</Typography>
+                            }
                         </Box>
                     </Grid>
                     <Grid item xs={2} display='flex' alignItems='center' flexDirection='column'>
                         <Typography variant="subtitle1">{`$${product.price}`}</Typography>
-                        {/* Editable */}
-                        <Button variant="text" color="secondary">
-                            Remover
-                        </Button>
+                        {
+                            editable && (
+                                <Button variant="text" color="secondary">
+                                    Remover
+                                </Button>
+                            )
+                        }
                     </Grid>
 
                 </Grid>
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,4 +1,5 @@
 import CartList from "@/components/cart/CartList"
+import OrderSummary from "@/components/cart/OrderSummary"
 import ShopLayout from "@/components/layouts/ShopLayout"
 import { Box, Button, Card, CardContent, Divider, Grid, Typography } from "@mui/material"
 
@@ -9,14 +10,14 @@ const CartPage = () => {
         <Typography variant='h1' component='h1'>Carrito</Typography>
         <Grid container>
             <Grid item xs={12} sm={7}>
-                <CartList/>
+                <CartList editable/>
             </Grid>
             <Grid item xs={12} sm={5}>
                 <Card className="summary-card">
                     <CardContent>
                         <Typography variant="h2">Orden</Typography>
                         <Divider sx={{ my:1 }}/>
-                        { /* OrderSummary */}
+                        <OrderSummary/>
                         <Box sx={{ mt: 3 }}>
                             <Button color="secondary" className="circular-btn" fullWidth>
                                 Checkout
